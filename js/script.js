e9a6bb4e66bf76df39998910bad29c6d922430c2
RahulNathani49 <nathanirahul49@gmail.com>
2021-12-06 11:31:30 -0500

    complete web

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function menurun() {
+    document.getElementById("links").style.display = "block";
+    document.getElementById("links").style.width = "480px";
+    document.getElementById("links").style.flexDirection = "column";
+    document.getElementById("navpart").style.flexDirection = "column";
+}
