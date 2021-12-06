5405b57ed92805dc95288c1f9cf03ccdb2c75aad
RahulNathani49 <nathanirahul49@gmail.com>
2021-12-06 11:37:43 -0500

    complete web

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 function menurun() {
     document.getElementById("links").style.display = "block";
+    document.getElementById("crossicon").style.display = "block";
+    document.getElementById("hamburger-icon").style.display = "none";
     document.getElementById("links").style.width = "480px";
     document.getElementById("links").style.flexDirection = "column";
     document.getElementById("navpart").style.flexDirection = "column";
+}
+
+function menuclose() {
+    document.getElementById("links").style.display = "none";
+    document.getElementById("crossicon").style.display = "none";
+    document.getElementById("hamburger-icon").style.display = "block";
 }
